5070cf3ab4c1a0c94e41441320c6dcae997d447e
MrZhao416 <1218392210@qq.com>
2019-06-01 09:49:03 +0800

    first commit

--- src/home/home.js
+++ src/home/home.js
@@ -9,19 +9,11 @@ class Home extends Component {
     constructor(props) {
         super(props)
     }
-<<<<<<< HEAD
-    // componentDidMount(){
-    //     axios.get("https://api1.34580.com/wx/Products/BigCategoryRequest?sourcetype=9").then((res)=>{
-    //         console.log(res.data);
-    //     })
-    // }
-=======
     componentDidMount(){
         axios.get("https://api1.34580.com/wx/Products/BigCategoryRequest?sourcetype=9").then((res)=>{
-            // console.log(res.data);/
+            console.log(res.data);
         })
     }
->>>>>>> 841194a99561f464229b7f328b809a346a65ed5a
     render() {
         console.log(this.props.home)
         return (
